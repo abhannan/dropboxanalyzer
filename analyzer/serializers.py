086bd4094721e341577251acee7c4df068aa0a73
Hannan <abhannan@hotmail.com>
2018-03-03 23:42:44 -0500

    fix file types to lower

--- analyzer/serializers.py
+++ analyzer/serializers.py
@@ -32,30 +32,30 @@ class UserUsageInfoSerializer(serializers.ModelSerializer):
     def get_files_categories(self, obj):
         file_categories = Counter(file.split(".")[-1] for file in obj.files_list)
 
-        img_extns = ['ai', 'bmp', 'gif', 'ico', 'jpeg', 'jpg', 'png', 'psd', 'svg', 'tif', 'tiff']
+        img_extns = ['ai', 'bmp', 'gif', 'ico', 'jpeg', 'jpg', 'png', 'psd', 'svg', 'tif', 'tiff', 'cr2']
         audio_extns = ['aif', 'cda', 'mid', 'midi', 'mp3', 'mpa', 'ogg', 'wav', 'wma', 'wpl']
         video_extns = ['3g2', '3gp', 'avi', 'flv', 'm4v', 'mkv', 'mov', 'mp4', 'mpg', 'mpeg', 'rm',
                        'swf', 'vob', 'wmv']
         docs_extns = ['ppt', 'pptx', 'key', 'odp', 'pps', 'ods', 'xlr', 'xls', 'xlsx', 'doc',
                       'docx', 'odt', 'pdf', 'rtf', 'txt', ]
 
-        categoriest = {'Images': 0, 'Videos': 0, 'Documents': 0, 'Others': 0, 'Audios': 0}
+        categories = {'Images': 0, 'Videos': 0, 'Documents': 0, 'Others': 0, 'Audios': 0}
 
         for key, value in file_categories.items():
-            if key in img_extns:
-                categoriest['Images'] += value
-            elif key in audio_extns:
-                categoriest['Audios'] += value
-            elif key in video_extns:
-                categoriest['Videos'] += value
-            elif key in docs_extns:
-                categoriest['Documents'] += value
+            if key.lower() in img_extns:
+                categories['Images'] += value
+            elif key.lower() in audio_extns:
+                categories['Audios'] += value
+            elif key.lower() in video_extns:
+                categories['Videos'] += value
+            elif key.lower() in docs_extns:
+                categories['Documents'] += value
             else:
-                categoriest['Others'] += value
-        return categoriest
+                categories['Others'] += value
+        return categories
 
     def get_files_categories_sizes(self, obj):
-        img_extns = ['ai', 'bmp', 'gif', 'ico', 'jpeg', 'jpg', 'png', 'psd', 'svg', 'tif', 'tiff']
+        img_extns = ['ai', 'bmp', 'gif', 'ico', 'jpeg', 'jpg', 'png', 'psd', 'svg', 'tif', 'tiff', 'cr2']
         audio_extns = ['aif', 'cda', 'mid', 'midi', 'mp3', 'mpa', 'ogg', 'wav', 'wma', 'wpl']
         video_extns = ['3g2', '3gp', 'avi', 'flv', 'm4v', 'mkv', 'mov', 'mp4', 'mpg', 'mpeg', 'rm',
                        'swf', 'vob', 'wmv']
@@ -70,13 +70,13 @@ class UserUsageInfoSerializer(serializers.ModelSerializer):
         categories_dict = {'Images': 0, 'Videos': 0, 'Documents': 0, 'Others': 0, 'Audios': 0}
 
         for key, value in categories_sizes_dict.items():
-            if key in img_extns:
+            if key.lower() in img_extns:
                 categories_dict['Images'] += value
-            elif key in audio_extns:
+            elif key.lower() in audio_extns:
                 categories_dict['Audios'] += value
-            elif key in video_extns:
+            elif key.lower() in video_extns:
                 categories_dict['Videos'] += value
-            elif key in docs_extns:
+            elif key.lower() in docs_extns:
                 categories_dict['Documents'] += value
             else:
                 categories_dict['Others'] += value
